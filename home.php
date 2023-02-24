52aa63fe424855d94ee2ab4334bd51386fc4dd8c
bigcogroup <106740511+bigcogroup@users.noreply.github.com>
2023-02-24 16:30:22 +0300

    Update home.php

--- home.php
+++ home.php
@@ -26,34 +26,72 @@
         $latitude = $_COOKIE['latitude'];
         $longitude = $_COOKIE['longitude'];
         
+        // Set the radius of the geofence in meters
+        $radius = 500;
         
-    
-        $sql_distance = $having = ''; 
-        if(!empty($distance_km) && !empty($latitude) && !empty($longitude)){ 
-            $radius_km = 0.5; 
-            $sql_distance = " ,(((acos(sin((".$latitude."*pi()/180)) * sin((`p`.`latitude`*pi()/180))+cos((".$latitude."*pi()/180)) * cos((`p`.`latitude`*pi()/180)) * cos(((".$longitude."-`p`.`longitude`)*pi()/180))))*180/pi())*60*1.1515*1.609344) as distance "; 
-             
-            $having = " HAVING (distance <= $radius_km) "; 
-             
-            $order_by = ' distance ASC '; 
-        }else{ 
-            $order_by = ' p.id DESC '; 
-        } 
-         
-        // Fetch places from the database 
-        $sql = "SELECT p.*".$sql_distance." FROM places p $having ORDER BY $order_by"; 
-        $query = $conn->query($sql); 
+        // Convert latitude and longitude to radians
+        $latitude_rad = deg2rad($latitude);
+        $longitude_rad = deg2rad($longitude);
         
-        if($query->num_rows > 0){ 
-            while($row = $query->fetch_assoc()){ 
-                $place = $row['id'];
-                $mysql = "SELECT * FROM product where place_id = $place";
-                $all_product = $conn->query($mysql);
-                $place = $row['id'];
-                $mysql = "SELECT * FROM product where place_id = $place";
-                $all_product = $conn->query($mysql);
+        // Earth's radius in meters
+        $earth_radius = 6371000;
+        
+        // Calculate the minimum and maximum latitude and longitude
+        $min_latitude = rad2deg($latitude_rad - ($radius / $earth_radius));
+        $max_latitude = rad2deg($latitude_rad + ($radius / $earth_radius));
+        $min_longitude = rad2deg($longitude_rad - ($radius / $earth_radius / cos($latitude_rad)));
+        $max_longitude = rad2deg($longitude_rad + ($radius / $earth_radius / cos($latitude_rad)));
+        
+        // Define the geofence as a polygon
+        $geofence = [
+            [$min_latitude, $min_longitude],
+            [$min_latitude, $max_longitude],
+            [$max_latitude, $max_longitude],
+            [$max_latitude, $min_longitude],
+            [$min_latitude, $min_longitude],
+        ];
+        
+        // Define the MySQL query to select locations within the geofence
+        $query = "SELECT * FROM places WHERE latitude BETWEEN " . $min_latitude . " AND " . $max_latitude . " AND longitude BETWEEN " . $min_longitude . " AND " . $max_longitude;
+        
+        // Execute the MySQL query and output the results
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+
+                $lat1 = $latitude; // Replace with the actual latitude
+                $lon1 = $longitude; // Replace with the actual longitude
+
+                // Set the latitude and longitude of the second point
+                $lat2 = $row["latitude"]; // Replace with the actual latitude
+                $lon2 = $row["longitude"]; // Replace with the actual longitude
+
+                // Earth's radius in meters
+                $earth_radius = 6371000;
+
+                // Convert latitude and longitude to radians
+                $lat1_rad = deg2rad($lat1);
+                $lon1_rad = deg2rad($lon1);
+                $lat2_rad = deg2rad($lat2);
+                $lon2_rad = deg2rad($lon2);
+
+                // Calculate the distance between the two points using the Haversine formula
+                $delta_lat = $lat2_rad - $lat1_rad;
+                $delta_lon = $lon2_rad - $lon1_rad;
+                $a = sin($delta_lat / 2) * sin($delta_lat / 2) + cos($lat1_rad) * cos($lat2_rad) * sin($delta_lon / 2) * sin($delta_lon / 2);
+                $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+                $distance = $earth_radius * $c;
+
+                // Output the distance in meters
+                echo "The distance between the two points is " . $distance . " meters.";
                 
+                echo "Location ID: " . $row["id"] . " - Latitude: " . $row["latitude"] . " - Longitude: " . $row["longitude"] . "<br>";
+                $place = $row['id'];
+                $mysql = "SELECT * FROM product where place_id = $place";
+                $all_product = $conn->query($mysql);
                 while($rows = mysqli_fetch_assoc($all_product)){
+
+
                     ?>
                     <div class="card">
                         <div class="image">
@@ -76,28 +114,13 @@
                     <?php
              
                     }
-                
+            
+            }
+        } else {
+            echo "No locations found within the geofence.";
+        }
+        ?>
     
-        ?> 
-            <div class="pbox"> 
-                <h4><?php echo $row['title']; ?></h4> 
-                <p><?php echo $row['address']; ?></p> 
-                <?php if(empty(!$row['distance'])){ ?> 
-                <p>Distance: <?php ($row['distance'])*1000; ?> Metres<p> 
-                <?php } ?> 
-            </div> 
-        <?php 
-            } 
-        }else{ 
-            echo '<h5>Place(s) not found...</h5>'; 
-        } 
-        
-        $place = $row['id'];
-        $mysql = "SELECT * FROM product where place_id = $place";
-        $all_product = $conn->query($mysql);
-    
-      
-    ?>
      
    </main>
    <script>
